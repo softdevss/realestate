3a2a5747dfb1a7749820e0a1320e6a5697ff64a6
Ralph <MY_NAME@example.com>
2021-08-25 15:43:55 +0800

    first commit

--- php_multiplelogin.sql
+++ php_multiplelogin.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2021 at 03:03 AM
+-- Generation Time: Aug 25, 2021 at 09:28 AM
 -- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- PHP Version: 8.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,39 @@ SET time_zone = "+00:00";
 --
 -- Database: `php_multiplelogin`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `houses`
+--
+
+CREATE TABLE `houses` (
+  `house_id` int(11) NOT NULL,
+  `house_type` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `rent_cost` varchar(11) NOT NULL,
+  `location` varchar(30) NOT NULL,
+  `location_description` text NOT NULL,
+  `max_capacity` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `houses`
+--
+
+INSERT INTO `houses` (`house_id`, `house_type`, `image`, `rent_cost`, `location`, `location_description`, `max_capacity`, `name`, `contact`, `status`) VALUES
+(44, 'YAZZI MODEL HOUSE - BROWN', 'Yazzi - Brown.PNG', '2Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Available'),
+(45, 'YAZZI MODEL HOUSE - GOLD', 'Yazzi - Gold.PNG', '3Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Available'),
+(46, 'YAZZI MODEL HOUSE - PINK', 'Yazzi - Pink.PNG', '3Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Available'),
+(47, 'YAZZI MODEL HOUSE - Midnight Blue', 'Yazzi - Midnight Blue.JPG', '3Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Available'),
+(48, 'NANNI MODEL HOUSE - Brown', 'Nanni - Brown.JPG', '3Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Available'),
+(49, 'NANNI MODEL HOUSE - Red', 'Nanni - Red.HEIC', '3Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Added'),
+(50, 'DAMARA MODEL HOUSE', 'Damara Model House.JPG', '3Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Available'),
+(51, 'CHENNEL MODEL HOUSE', 'Chennel Model House.JPG', '3Million', 'Sta. Maria, Guiwan and Mercede', 'Our finished SOLD Houses! ', '60sqm', 'Sakura Ann Yanson', '2147483647', 'Added');
 
 -- --------------------------------------------------------
 
@@ -52,6 +85,12 @@ INSERT INTO `masterlogin` (`id`, `username`, `email`, `password`, `role`) VALUES
 --
 
 --
+-- Indexes for table `houses`
+--
+ALTER TABLE `houses`
+  ADD PRIMARY KEY (`house_id`);
+
+--
 -- Indexes for table `masterlogin`
 --
 ALTER TABLE `masterlogin`
@@ -60,6 +99,12 @@ ALTER TABLE `masterlogin`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `houses`
+--
+ALTER TABLE `houses`
+  MODIFY `house_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `masterlogin`
