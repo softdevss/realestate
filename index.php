9918c09ec96919e514efc7feeb7cbb6a527b3922
Ralph lawrence <rralphlawrence@gmail.com>
2021-10-03 21:43:10 +0800

    vidoes

--- index.php
+++ index.php
@@ -179,7 +179,7 @@
 		
     <div class="vid-wrapper">
       <div class="vid-container">
-         <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fgmanews%2Fvideos%2F1096893304403585%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+      <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fsoftwaredevsystem%2Fvideos%2F681758816549797%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
       </div>        
     </div> 
       <!-- ==== ABOUT ==== -->
